93ff7fa19deed645248e5501db6a3be6e3584d85
alexTprog <alexander.t1780@gmail.com>
2022-03-31 19:04:28 -0500

    db config

--- app.py
+++ app.py
@@ -1,7 +1,22 @@
+import os
 from flask import Flask
 
+#Crear y configurar el app
 app = Flask(__name__)
 
+app.config.from_mapping(
+    SECRET_KEY='ga',
+    DATABASE= os.path.join(app.instance_path,'bikeshop.sqlite')    
+)
+
+try:
+    os.makedirs(app.instance_path)
+except OSError():
+    pass
+
+
+
+# una pagia simple
 @app.route('/')
 def index():
-    return "Todo en Orden"
+    return 'Hello, Wordl!'
